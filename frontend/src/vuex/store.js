90ad143353d5fdc8d640039ab5636395d0f3b42c
ohazyi_cloud <291045048@qq.com>
2018-01-25 10:39:19 +0800

    :tada: first commit

---
+++ frontend/src/vuex/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    id: 0,
+    name: '',
+    size: '',
+    intro: '',
+    author: '',
+    time: '',
+    url: '',
+    course_code: '',
+    // state relating to login
+    user_name: '',
+    is_login: false,
+    // state relating to debug
+    dev: false
+  },
+  mutations: {
+  }
+})
+export default store
